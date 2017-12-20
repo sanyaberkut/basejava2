f37405509a8e32b130f54aba0ff6cb2b847df221
Progress <sanyaberkut@yandex.ru>
2017-12-20 21:02:48 +0200

    homework 2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,42 +3,67 @@
  */
 import java.util.Arrays;
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = 0;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
+    public void clear() {
         for(int i = 0; i<size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
 
-        if( size < storage.length ) {
-            storage[size] = r;
-            size++;
+        if(get(r.getUuid()) == null) {
+            if( size < storage.length ) {
+                storage[size] = r;
+                size++;
+                System.out.println("Добавлен новый элемент");
+            } else {
+                System.out.println("Хранилище заполнено");
+            }
         } else {
-            System.out.println("Хранилище заполнено");
+            update(r.getUuid(), r);
         }
     }
 
-    Resume get(String uuid) {
+    public void update(String uuid, Resume r) {
+        if( get(r.getUuid()) == null) {
+            System.out.println("Резюме не найдено с данным идентификатор");
+        } else {
+            for(int i = 0; i<size; i++) {
+                if (storage[i] != null && storage[i].toString().equals(uuid)) {
+                    storage[i] = r;
+                    System.out.println("Резюме обновлено");
+                    return;
+                }
+            }
+        }
+    }
+
+    public Resume get(String uuid) {
         for(int i = 0; i<size; i++) {
             if (storage[i] != null && storage[i].toString().equals(uuid)) {
-                    return storage[i];
+                return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid) {
-        for(int i = 0; i<size; i++) {
-            if (storage[i] != null && storage[i].toString().equals(uuid)) {
-                 // переносим в позицию где был удален элемент с послдеднего елемента масива
-                storage[i]=storage[size-1];
-                storage[size-1] = null;
-                size--;
+    public void delete(String uuid) {
+        if( get(uuid) == null) {
+            System.out.println("Резюме не найдено с данным идентификатор");
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (storage[i] != null && storage[i].toString().equals(uuid)) {
+                    // переносим в позицию где был удален элемент с послдеднего елемента масива
+                    storage[i] = storage[size - 1];
+                    storage[size - 1] = null;
+                    size--;
+                    System.out.println("Резюме удалено");
+                    return;
+                }
             }
         }
     }
@@ -46,14 +71,13 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] storagesNew = new Resume[size];
-
         System.arraycopy(storage, 0, storagesNew, 0, size);
         return storagesNew;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 }
